61a017535b9623208d0bbd3143c56d0ef1324296
Carl Windhausen <cjwindhausen@gmail.com>
2013-11-01 10:27:43 -0400

    4.4 OctoKat KRT16M

--- oct.mk
+++ oct.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
 # Inherit some common OCT stuff.
 $(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/oct_jflteusc/jflteusc:4.3.1/JLS36I/user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.3.1 JLS36I release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteusc TARGET_DEVICE=jflteusc BUILD_FINGERPRINT="samsung/oct_jflteusc/jflteusc:4.4/KRT16M/user/release-keys" PRIVATE_BUILD_DESC="jflteusc-user 4.4 KRT16M release-keys"
 
 PRODUCT_DEVICE := jflteusc
 PRODUCT_NAME := oct_jflteusc
